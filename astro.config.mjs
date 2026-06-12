be6d1aa4a2543d17832d8c93235ca5c0649d7226
zhangxielin <zhangxielin@gmail.com>
2026-06-13 00:44:43 +0800

    新增sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://chachaphoto.uk',
+
+  integrations: [
+    sitemap()
+  ]
+});
